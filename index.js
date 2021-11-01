8379cdc5591428986eded22147c7b260dd30162c
Mehtab Alam <mehtaba728@mail.com>
2021-11-01 16:42:20 +0500

    uploading the initial version working ok with frontend upto live video screen

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+let users = [];
+const port = 3001;
+
+const addUser = (userName, roomId) => {
+  users.push({
+    userName: userName,
+    roomId: roomId,
+  });
+};
+
+const userLeave = (userName) => {
+  users = users.filter((user) => user.userName != userName);
+};
+
+const getRoomUsers = (roomId) => {
+  return users.filter((user) => user.roomId == roomId);
+};
+
+app.get("/", (req, res) => {
+  res.send("Welcome to NodeJS and ExpressJS");
+});
+
+io.on("connection", (socket) => {
+  console.log("someone connected");
+  socket.on("join-room", ({ roomId, userName }) => {
+    console.log("user joined room");
+    console.log(userName);
+    console.log(roomId);
+    socket.join(roomId);
+    addUser(userName, roomId);
+    socket.to(roomId).emit("user-connected ", userName);
+
+    io.to(roomId).emit("all-users", getRoomUsers(roomId));
+
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+      socket.leave(roomId);
+      userLeave(userName);
+      io.to(roomId).emit("all-users", getRoomUsers(roomId));
+    });
+  });
+});
+
+server.listen(port, () => {
+  console.log("Zoom clone API listening on localhost:3001");
+});
