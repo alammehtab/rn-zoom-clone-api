aa2adfb5dabdde23bc2b47c73bbbebb7779a39fc
mehtabalam <mehtaba728@gmail.com>
2022-02-10 02:09:40 +0500

    - initial commit

--- index.js
+++ index.js
@@ -31,11 +31,12 @@ io.on("connection", (socket) => {
     console.log("user joined room");
     console.log(userName);
     console.log(roomId);
-    socket.join(roomId);
-    addUser(userName, roomId);
-    socket.to(roomId).emit("user-connected ", userName);
-
-    io.to(roomId).emit("all-users", getRoomUsers(roomId));
+    if (roomId && userName) {
+      socket.join(roomId);
+      addUser(userName, roomId);
+      socket.to(roomId).emit("user-connected ", userName);
+      io.to(roomId).emit("all-users", getRoomUsers(roomId));
+    }
 
     socket.on("disconnect", () => {
       console.log("disconnected");
